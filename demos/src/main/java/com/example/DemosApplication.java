d629e9f0429feed4837a6fb42668639ccef24ab6
Javier <jmartin@grupoloyal.com>
2021-05-17 18:47:36 +0200

    Ejemplo

--- demos/src/main/java/com/example/DemosApplication.java
+++ demos/src/main/java/com/example/DemosApplication.java
@@ -1,13 +1,19 @@
 package com.example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemosApplication {
+public class DemosApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemosApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hola mundo");
 	}
 
 }
