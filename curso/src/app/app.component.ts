552dd2d1ba56c50e4db097fe898a6314c7112c93
Javier <jmartin@grupoloyal.com>
2021-05-28 17:51:39 +0200

    Logger

--- curso/src/app/app.component.ts
+++ curso/src/app/app.component.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
-import { LoggerService } from 'src/my-core';
+// import { ERROR_LEVEL, LoggerService } from 'src/my-core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // providers: [
+  //   LoggerService,
+  //   { provide: ERROR_LEVEL, useValue: 4 },
+  // ],
 })
 export class AppComponent {
   title = 'hola toledo';
 
-  constructor(out: LoggerService) {
-    out.error('Es un error');
-    out.warn('Es un warn');
-    out.info('Es un info');
-    out.log('Es un log');
-  }
+  // constructor(out: LoggerService) {
+  //   out.error('Es un error');
+  //   out.warn('Es un warn');
+  //   out.info('Es un info');
+  //   out.log('Es un log');
+  // }
 }
