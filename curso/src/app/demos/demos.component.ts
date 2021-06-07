5a7ad8ca47ef5cf8268b56b761f4ab07b9bad99e
Javier <jmartin@grupoloyal.com>
2021-06-07 16:32:48 +0200

    Clase del componente

--- curso/src/app/demos/demos.component.ts
+++ curso/src/app/demos/demos.component.ts
@@ -10,7 +10,53 @@ import { NotificationService, NotificationType } from '../common-services';
 export class DemosComponent implements OnInit, OnDestroy {
   private suscriptor: Unsubscribable | undefined;
 
+  private nombre: string = 'mundo';
+  listado = [
+    { id: 1, nombre: 'Toledo'},
+    { id: 2, nombre: 'albacete'},
+    { id: 3, nombre: 'CUENCA'},
+    { id: 4, nombre: 'Ciudad real'},
+  ];
+  idProvincia = 2;
+
+  resultado: string | null = null;
+  visible = true;
+  estetica = { importante: true, error: false, urgente: true };
+  fontsize = 24;
+
   constructor(public vm: NotificationService) { }
+
+  public get Nombre(): string { return this.nombre; }
+  public set Nombre(valor: string) {
+    if(this.nombre === valor) return;
+    this.nombre = valor;
+  }
+
+  public saluda(): void {
+    this.resultado = `Hola ${this.nombre}`;
+  }
+  public despide(): void {
+    this.resultado = `Adios ${this.nombre}`;
+  }
+  public di(algo: string): void {
+    this.resultado = `Dice ${algo}`;
+  }
+
+  cambia() {
+    this.visible = !this.visible;
+    this.estetica.importante = !this.estetica.importante;
+    this.estetica.error = !this.estetica.error;
+  }
+
+  calculo(a: number, b: number): number {
+    return a + b;
+  }
+
+  add(provincia: string) {
+    const id = this.listado.length ? this.listado[this.listado.length - 1].id + 1 : 1;
+    this.listado.push({ id, nombre: provincia });
+    this.idProvincia = id;
+  }
 
   ngOnInit(): void {
     this.suscriptor = this.vm.Notificacion.subscribe(n => {
