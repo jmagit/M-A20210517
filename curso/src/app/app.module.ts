6522c82c0d1fb90c6c622df4c5fd7923681f1414
Javier <jmartin@grupoloyal.com>
2021-06-08 17:47:33 +0200

    Solución

--- curso/src/app/app.module.ts
+++ curso/src/app/app.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { environment } from 'src/environments/environment';
+
+// Cargar idioma --------------------------------------------------------
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+import localeEsExtra from '@angular/common/locales/extra/es';
+
+registerLocaleData(localeEs, 'es', localeEsExtra);
+// ---------------------------------------------------------------------
+
 import { ERROR_LEVEL, LoggerService, MyCoreModule } from 'src/my-core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,11 +19,12 @@ import { CommonServicesModule } from './common-services';
 import { MainModule } from './main';
 import { BotonesComponent, DemosComponent } from './demos/demos.component';
 import { DinamicoComponent } from './dinamico/dinamico.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemosComponent, BotonesComponent, DinamicoComponent,
+    DemosComponent, BotonesComponent, DinamicoComponent, CalculadoraComponent,
   ],
   imports: [
     BrowserModule, FormsModule,
@@ -24,6 +34,7 @@ import { DinamicoComponent } from './dinamico/dinamico.component';
   providers: [
     LoggerService,
     { provide: ERROR_LEVEL, useValue: environment.ERROR_LEVEL },
+    { provide: LOCALE_ID, useValue: 'es-ES' },
   ],
   bootstrap: [AppComponent]
 })
--- curso/src/app/dinamico/dinamico.component.ts
+++ curso/src/app/dinamico/dinamico.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CalculadoraComponent } from '../calculadora/calculadora.component';
 import { DemosComponent } from '../demos/demos.component';
 import { HomeComponent } from '../main/home/home.component';
 
@@ -9,8 +10,9 @@ import { HomeComponent } from '../main/home/home.component';
 })
 export class DinamicoComponent implements OnInit {
   menu = [
-    {texto: 'inicio', componente: HomeComponent},
     {texto: 'demos', componente: DemosComponent },
+    {texto: 'inicio', componente: HomeComponent},
+    {texto: 'calculadora', componente: CalculadoraComponent },
   ];
   actual = this.menu[0].componente;
 
