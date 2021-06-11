37c0fdafbb74dc54ded03eb19a96f123dbf47877
Javier <jmartin@grupoloyal.com>
2021-06-11 18:32:57 +0200

    Config MVC

--- demos/src/main/java/com/example/WebConfig.java
+++ demos/src/main/java/com/example/WebConfig.java
@@ -1,11 +1,15 @@
 package com.example;
 
+import java.time.Duration;
+
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -39,6 +43,13 @@ public class WebConfig implements WebMvcConfigurer {
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
 	}
+	
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+            .addResourceLocations("/public/**", "classpath:/static/")
+            .setCacheControl(CacheControl.maxAge(Duration.ofDays(365)));
+    }
 	
 //    @Override
 //    public void addCorsMappings(CorsRegistry registry) {
